fa735923b314fa981629e38e02184b57af66e907
Prem Muthedath <prem.muthedath@gmail.com>
2013-11-29 19:19:48 +0530

    Minor name changes and edits.

--- src/tests/timer/AllTests.java
+++ src/tests/timer/AllTests.java
@@ -8,12 +8,12 @@ public class AllTests {
 	}
 
 	public void testList() throws Exception {
-		MethodsTimer tester= new MethodsTimer(ListSearch.class.getDeclaredMethods());
-		tester.report();		
+		MethodsTimer timer= new MethodsTimer(ListSearch.class.getDeclaredMethods());
+		timer.report();		
 	}
 
 	public void testSetVsArray() throws Exception {
-		MethodsTimer tester= new MethodsTimer(SetVsArrayList.class.getDeclaredMethods());
-		tester.report();		
+		MethodsTimer timer= new MethodsTimer(SetVsArrayList.class.getDeclaredMethods());
+		timer.report();		
 	}
 }
