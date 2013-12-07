349ef9e8dc22a146443080249c4a0c0d9e9588c4
Prem Muthedath <prem.muthedath@gmail.com>
2013-12-07 15:36:19 +0530

    Renamed a test method.

--- src/tests/timer/AllTests.java
+++ src/tests/timer/AllTests.java
@@ -3,11 +3,11 @@ package timer;
 public class AllTests {
 	public static void main(String[] args) throws Exception {
 		AllTests tests=new AllTests();
-		tests.testList();
+		tests.testListSearch();
 		tests.testSetVsArray();
 	}
 
-	public void testList() throws Exception {
+	public void testListSearch() throws Exception {
 		MethodsTimer timer= new MethodsTimer(ListSearch.class.getDeclaredMethods());
 		timer.report();		
 	}
