4f9c3e079f1fbdc04ed85fbee93649355257b6b0
Prem Muthedath <prem.muthedath@gmail.com>
2014-06-07 16:52:48 +0530

    Order -- removed redundant methods.

--- src/main/timer/reporting/ordering/Order.java
+++ src/main/timer/reporting/ordering/Order.java
@@ -4,32 +4,22 @@ import timer.framework.Report;
 
 import timer.reporting.base.View;
 
-import java.util.Comparator;
-
 public enum Order {
 	BY_SIZE, BY_METHOD;
 
 	public Report report()  {
 		switch(this)  {
-			case BY_SIZE: return new OrderedReport(sizeComparator()) {
+			case BY_SIZE: return new OrderedReport(TestInstance.sizeComparator()) {
 				void export(TestInstance testInstance, Timing timing, View view) {
 					testInstance.exportBySize(timing, view);
 				}
 			};
-			case BY_METHOD: return new OrderedReport(methodComparator()) {
+			case BY_METHOD: return new OrderedReport(TestInstance.methodComparator()) {
 				void export(TestInstance testInstance, Timing timing, View view) {
 					testInstance.exportByMethod(timing, view);
 				}
 			};					
 		}
 		throw new RuntimeException("Unknown Order: "+this);
-	}
-
-	private Comparator<TestInstance> sizeComparator() {
-		return TestInstance.sizeComparator();
-	}
-
-	private Comparator<TestInstance> methodComparator() {
-		return TestInstance.methodComparator();
 	}
 }
