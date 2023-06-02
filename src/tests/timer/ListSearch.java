19c541c5e0a764abbeb318edd406ea0fb44e9c6e
Prem Muthedath <prem.muthedath@gmail.com>
2023-06-02 16:11:19 +0530

    ` src/tests/timer/ListSearch.java`, `src/tests/timer/SetVsArrayList.java`: + comments.

--- src/tests/timer/ListSearch.java
+++ src/tests/timer/ListSearch.java
@@ -3,17 +3,28 @@ package timer;
 import java.util.ArrayList;
 import java.util.List;
 
+/*  Prem added this import. */
 import timer.framework.TimingTests;
 
-/* java 1.7 API reference
- * https://docs.oracle.com/en/java/javase/17/docs/api/
+/** Timing tests encapsulated for list search.
  *
- * original source of this code: Kent Beck's 'Implementation Patterns' book.
- * Prem slightly modified Beck's code to suit his design here.
+ *  Each public method in this class is a timing test method.
+ *  All timing tests have to be public methods.
  *
- * author: Kent Beck, with slight modifications by Prem Muthedath
+ *  Terminology:
+ *  Note that `timing test` is same as `timing test method` or `test method`.  
+ *  All these terms refer to a public method in this class.
+ *
+ *  original source of this code: Kent Beck's 'Implementation Patterns' book.
+ *  Prem slightly modified Beck's code to suit his design here.
+ *
+ *  java 1.7 API reference
+ *  https://docs.oracle.com/en/java/javase/17/docs/api/
+ *
+ *  author: Kent Beck, with comments and slight modifications by Prem Muthedath
  */
-public class ListSearch extends TimingTests {
+public class ListSearch extends TimingTests { /* Prem added this inheritance */
+  /* Kent Beck's code. */
   private List<Integer> numbers;
   private int probe;
 
@@ -24,14 +35,17 @@ public class ListSearch extends TimingTests {
     probe= size / 2;
   }
 
+  /* timing test method for list search. This method comes from Kent Beck. */
   public void search() {
     numbers.contains(probe);
   }
 
+  /* Prem added this method to run the timing tests in his framework. */
   protected int size()  {
     return numbers.size();
   }
 
+  /* Prem added this method to run the timing tests in his framework. */
   protected TimingTests copy() {
     return new ListSearch(size());
   }
--- src/tests/timer/SetVsArrayList.java
+++ src/tests/timer/SetVsArrayList.java
@@ -5,17 +5,28 @@ import java.util.HashSet;
 import java.util.Collection;
 import java.util.Iterator;
 
+/*  Prem added this import. */
 import timer.framework.TimingTests;
 
-/* java 1.7 API reference
- * https://docs.oracle.com/en/java/javase/17/docs/api/
+/** Timing tests encapsulated for Set vs ArrayList comparison.
  *
- * original source of this code: Kent Beck's 'Implementation Patterns' book.
- * Prem slightly modified Beck's code to suit his design here.
+ *  Each public method in this class is a timing test method.
+ *  All timing tests have to be public methods.
  *
- * author: Kent Beck, with slight modifications by Prem Muthedath
+ *  Terminology:
+ *  Note that `timing test` is same as `timing test method` or `test method`.  
+ *  All these terms refer to a public method in this class.
+ *
+ *  original source of this code: Kent Beck's 'Implementation Patterns' book.
+ *  Prem slightly modified Beck's code to suit his design here.
+ *
+ *  java 1.7 API reference
+ *  https://docs.oracle.com/en/java/javase/17/docs/api/
+ *
+ *  author: Kent Beck, with comments and slight modifications by Prem Muthedath
  */
-public class SetVsArrayList extends TimingTests {
+public class SetVsArrayList extends TimingTests {/* Prem added this inheritance */
+  /* Kent Beck's code. */
   private Collection<String> set;
   private Collection<String> arrayList;
 
@@ -32,39 +43,53 @@ public class SetVsArrayList extends TimingTests {
     probe= String.format("a%d", size / 2);
   }
 
+  /* Prem added this method to run the timing tests in his framework. */
   protected int size()  {
     return arrayList.size();
   }
 
+  /* Prem added this method to run the timing tests in his framework. */
   protected TimingTests copy() {
     return new SetVsArrayList(size());
   }
 
+  /* timing test for set membership. This method comes from Kent Beck. */
   public void setMembership() {
     set.contains(probe);
   }
 
+  /* timing test for array list membership; written by Kent Beck. */
   public void arrayListMembership() {
     arrayList.contains(probe);
   }
 
+  /* timing test method for set iteration; written by Kent Beck. */
   public void setIteration() {
     Iterator<String> all= set.iterator();
     while (all.hasNext())
       all.next();
   }
 
+  /* timing test method for array list iteration; written by Kent Beck. */
   public void arrayListIteration() {
     Iterator<String> all= arrayList.iterator();
     while (all.hasNext())
       all.next();
   }
 
+  /* timing test for set modification; written by Kent Beck.
+   * NOTE: this operation ensures that the original collection remains 
+   * unchanged.  this is important to avoid dependencies between tests.
+   */
   public void setModification() {
     set.add("b");
     set.remove("b");
   }
 
+  /* timing test method for array list modification; written by Kent Beck.
+   * NOTE: this operation ensures that the original collection remains 
+   * unchanged.  this is important to avoid dependencies between tests.
+   */
   public void arrayListModification() {
     arrayList.add("b");
     arrayList.remove("b");
