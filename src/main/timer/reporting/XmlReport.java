049d88747766ed202eaa60432e750be6045500d1
Prem Muthedath <prem.muthedath@gmail.com>
2023-06-05 17:43:36 +0530

    src/main/timer/reporting/XmlReport.java: redesigned in a major way -- now uses inner classes for xml generation, and we only print once the whole xml has been generated.

--- src/main/timer/reporting/XmlReport.java
+++ src/main/timer/reporting/XmlReport.java
@@ -6,71 +6,203 @@ package timer.reporting;
  * author: Prem Muthedath
  */
 public class XmlReport extends Report {
-  /* define couple of string constants. */
-  private final String TESTS_LABEL="timing-tests";
-  private final String TIMINGS_LABEL="method-timings";
+  private final String prolog="<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>";
+  private Tag root;
 
   public XmlReport(OrderedResults results) {
     super(results);
   }
 
   void viewTitle(String testClass) {
-    System.out.println("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>");
-    System.out.println(this.attrTag(this.TESTS_LABEL, "class", testClass));
-    System.out.println(this.attrTag(this.TIMINGS_LABEL, "units", "nanoseconds"));
+    this.setRoot(new Tag("timing-tests", new TagAttribute("class", testClass)));
+    System.out.println(this.prolog);
   }
 
-  void viewFooter() {
-    System.out.println(this.closeTag(this.TIMINGS_LABEL));
-    System.out.println(this.closeTag(this.TESTS_LABEL));
+  private void setRoot(Tag root) {
+    this.root=root;
   }
 
   protected void viewBySize(int[] sizes, double[][] timings) {
+    Xml[] result = new Xml [sizes.length];
     for (int i=0; i < sizes.length; i++)
-      System.out.println(this.sizeElements(sizes[i], timings[i]));
+      result[i] = this.sizeElements(sizes[i], timings[i]);
+    this.print(result);
   }
 
-  private String sizeElements(int size, double[] timings) {
+  private Xml sizeElements(int size, double[] timings) {
     String[] methods = super.methods();
-    String result = "\n";
+    Xml[] result = new Xml [timings.length];
     for (int i=0; i < timings.length; i++)
-      result += this.methodElement(methods[i], timingElement(timings[i])) + "\n";
+      result[i] = this.methodElement(methods[i], timingElement(timings[i]));
     return this.sizeElement(size, result);
   }
 
-  protected void viewByMethod(String[] methods, double[][] timings) {
-    for (int i=0; i < methods.length; i++)
-      System.out.println(this.methodElements(methods[i], timings[i]));
+  private void print(Xml[] xmls) {
+    Tag dataRoot = new Tag("method-timings", new TagAttribute("units", "nanoseconds"));
+    Xml xml = new XmlElements(root, new XmlElements(dataRoot, xmls));
+    System.out.println(xml);
   }
 
-  private String methodElements(String method, double[] timings) {
+  protected void viewByMethod(String[] methods, double[][] timings) {
+    Xml[] result = new Xml [methods.length];
+    for (int i=0; i < methods.length; i++)
+      result[i] = this.methodElements(methods[i], timings[i]);
+    this.print(result);
+  }
+
+  private Xml methodElements(String method, double[] timings) {
     int[] sizes = super.sizes();
-    String result = "\n";
+    Xml[] result = new Xml [timings.length];
     for (int i=0; i < timings.length; i++)
-      result += this.sizeElement(sizes[i], timingElement(timings[i])) + "\n";
+      result[i] = this.sizeElement(sizes[i], timingElement(timings[i]));
     return this.methodElement(method, result);
   }
 
-  private String sizeElement(int size, String value) {
-    String sizeTag = "<" + String.format("size value=\"%s\"", size) + ">";
-    return sizeTag + value + "</size>";
+  private XmlElements sizeElement(int size, Xml[] xmls) {
+    Tag tag = new Tag("size", new TagAttribute("value", String.format("%s", size)));
+    return new XmlElements(tag, xmls);
   }
 
-  private String methodElement(String method, String value) {
-    String methodTag = "<" + String.format("method name=\"%s\"", method) + ">";
-    return methodTag + value + "</method>";
+  private XmlElements sizeElement(int size, Xml xml) {
+    return this.sizeElement(size, new Xml[] { xml });
   }
 
-  private String timingElement(double timing) {
-    return "<timing>" + String.format("%.2f", timing) + "</timing>";
+  private XmlElements methodElement(String method, Xml[] xmls) {
+    Tag tag = new Tag("method", new TagAttribute("name", String.format("%s", method)));
+    return new XmlElements(tag, xmls);
   }
 
-  private String attrTag(String name, String attrName, String attrValue) {
-    return "<" + name + " " + attrName + "=\"" + attrValue + "\">";
+  private XmlElements methodElement(String method, Xml xml) {
+    return this.methodElement(method, new Xml[] { xml });
   }
 
-  private String closeTag(String name) {
-    return "</" + name + ">";
+  private XmlElement timingElement(double timing) {
+    return new XmlElement(new Tag("timing"), String.format("%.2f", timing));
   }
+
+  /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+  /* +++++++++++++++++ inner claases for XML generation ++++++++++++++++++++ */
+  /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+  private abstract class Xml {
+    private Tag tag;
+
+    private Xml(Tag tag) {
+      this.tag = tag;
+    }
+
+    public String toString() {
+      return this.tag.toString(this);
+    }
+
+    abstract String toString(String open, String close);
+  }
+
+  /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+  private class XmlElement extends Xml {
+    private String value;
+
+    private XmlElement(Tag tag, String value) {
+      super(tag);
+      /* NOTE: see /u/ ernest_k @ https://tinyurl.com/4jk52rea (so)
+       * instead of `trim()`, which trims everything, including "\n", you could 
+       * use the code below to trim just whitespace (leading & trailing):
+       *
+       *    this.value = value.replaceAll("(^ +)|( +$)", "");
+       */
+      this.value = value.trim();
+    }
+
+    String toString(String open, String close) {
+      return open + this.value + close;
+    }
+  }
+
+  /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+  private class XmlElements extends Xml {
+    private Tag tag;
+    private Xml[] children;
+
+    private XmlElements(Tag tag, Xml[] children) {
+      super(tag);
+      this.children = children;
+    }
+
+    private XmlElements(Tag tag, Xml child) {
+      super(tag);
+      this.children = new Xml[] { child };
+    }
+
+    String toString(String open, String close) {
+      String result=adfix();
+      for (int i=0; i < children.length; i++)
+        result+=children[i].toString() + adfix();
+      return open + result + close;
+    }
+
+    private String adfix() {
+      int size = this.children.length;
+      if (size > 1) return "\n";
+      if (size == 0) return "";
+      if (children[0] instanceof XmlElements) return "\n";
+      return "";
+    }
+  }
+
+  /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+  private class Tag {
+    private TagAttribute[] attributes;
+    private String name;
+
+    private Tag(String name, TagAttribute[] attributes) {
+      this.name = name.trim();
+      this.attributes = attributes;
+    }
+
+    private Tag(String name, TagAttribute attribute) {
+      this.name = name.trim();
+      this.attributes = new TagAttribute[] { attribute };
+    }
+
+    private Tag(String name) {
+      this(name, new TagAttribute("", ""));
+    }
+
+    private String open() {
+      if (name == "")
+        throw new IllegalArgumentException("tag name can not be empty");
+      String attributesString = "";
+      for (int i=0; i < this.attributes.length; i++)
+        attributesString += this.attributes[i].toString();
+      return "<" + name + attributesString + ">";
+    }
+
+    private String close() {
+      if (name == "")
+        throw new IllegalArgumentException("tag name can not be empty");
+      return "</" + name + ">";
+    }
+
+    private String toString(Xml xml) {
+      return xml.toString(open(), close());
+    }
+  }
+
+  /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+  private class TagAttribute {
+    private String name;
+    private String value;
+
+    private TagAttribute(String name, String value) {
+      this.name = name.trim();
+      this.value = value.trim();
+    }
+
+    public String toString() {
+      if (name == "") return "";
+      return " " + name + "=\"" + value + "\"";
+    }
+  }
+
+  /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 }
